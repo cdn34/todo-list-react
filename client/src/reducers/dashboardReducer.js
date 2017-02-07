dd575723056dc7c8e49dc26d497ab9d80c747c7f
cdn34 <carlos.nogueira04@gmail.com>
2017-02-07 22:09:28 +0000

    Added reducer composition to dashboard reducer

--- client/src/reducers/dashboardReducer.js
+++ client/src/reducers/dashboardReducer.js
@@ -14,38 +14,44 @@ const dashboardReducer = (state = initialState.dashboard, action) => {
             const removeLists = state.lists.filter(item => item._id!=action.list._id);
             return Object.assign({}, state, {lists: removeLists});
         case ADD_TODO:
-            const addTodosLists = state.lists.map(list => {
-                if(list._id !== action.payload._id)
-                    return list;
-                const last = action.payload.list.todos.length-1;
-                const todosArr = list.todos.concat([action.payload.list.todos[last]]);
-                return {
-                    ...list,
-                    todos: todosArr
-                }
-            });
+            const addTodosLists = state.lists.map(list => todoReducer(list, action));
             return Object.assign({}, state,{lists: addTodosLists});
         case REMOVE_TODO:
-            const removeTodoLists = state.lists.map(list => {
-                if(list._id !== action.payload.listId)
-                    return list;
-                let todoIndex = 0;
-                list.todos.forEach((todo, index) => { 
-                    if(todo._id == action.payload.todoId)
-                        todoIndex = index;
-                });
-                const last = list.todos.length-1;
-                //const todosArr = todoIndex < last ? list.todos.slice(0,todoIndex).concat(list.todos.slice(todoIndex+1)) : list.todos.slice(0,todoIndex);
-                const todosArr = todoIndex < last ? [...list.todos.slice(0,todoIndex), ...list.todos.slice(todoIndex+1)] : list.todos.slice(0,todoIndex);
-                return {
-                    ...list,
-                    todos: todosArr
-                }
-            })
+            const removeTodoLists = state.lists.map(list => todoReducer(list, action));
             return Object.assign({}, state,{lists: removeTodoLists});
-        
         default:
             return state;
+    }
+}
+
+
+const todoReducer = (state, action) => {
+    switch(action.type){
+        case ADD_TODO:
+            if(state._id !== action.payload._id)
+                return state;
+            const lastTodo = action.payload.list.todos.length-1;
+            //const addTodosArr = state.todos.concat([action.payload.list.todos[lastTodo]]);
+            const addTodosArr = [...state.todos, action.payload.list.todos[lastTodo]];
+            return {
+                ...state,
+                todos: addTodosArr
+            }
+        case REMOVE_TODO:
+            if(state._id !== action.payload.listId)
+                return state;
+            let todoIndex = 0;
+            state.todos.forEach((todo, index) => { 
+                if(todo._id == action.payload.todoId)
+                    todoIndex = index;
+            });
+            const last = state.todos.length-1;
+            //const todosArr = todoIndex < last ? state.todos.slice(0,todoIndex).concat(state.todos.slice(todoIndex+1)) : state.todos.slice(0,todoIndex);
+            const removeTodosArr = todoIndex < last ? [...state.todos.slice(0,todoIndex), ...state.todos.slice(todoIndex+1)] : state.todos.slice(0,todoIndex);
+            return {
+                ...state,
+                todos: removeTodosArr
+            }
     }
 }
 
